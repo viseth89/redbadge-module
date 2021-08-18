5314ccc9e788909e121bf8fa762bec899cee605d
viseth89 <viseth89@gmail.com>
2021-08-18 18:59:14 -0500

    Finished with 2.02

---
+++ app/01-types.ts
@@ -0,0 +1,12 @@
+let username: string = "instructor";
+let password: string = "Letmein1234!"
+let instructorId: number = 5;
+
+// let password: string;
+let phoneNumber: string;
+let email: string;
+let socialSecurity: number;
+let isMarried: boolean;
+
+let greeting = "Hello";
+password = 1234;
---
+++ js/01-types.js
@@ -0,0 +1,11 @@
+var username = "instructor";
+var password = "Letmein1234!";
+var instructorId = 5;
+// let password: string;
+var phoneNumber;
+var email;
+var socialSecurity;
+var isMarried;
+var greeting = "Hello";
+password = 1234;
+//# sourceMappingURL=01-types.js.map
