7674d5cc60b98f9ce6a59a06117eb2bf6a5141bf
viseth89 <viseth89@gmail.com>
2021-08-18 19:16:21 -0500

    Finished 2.03, enums is interesting

--- app/01-types.ts
+++ app/01-types.ts
@@ -9,4 +9,47 @@ let socialSecurity: number;
 let isMarried: boolean;
 
 let greeting = "Hello";
-password = 1234;
+// password = 1234;
+
+let age: number = 40;
+let currentYear: number = 2018;
+
+let companyName: string;
+companyName = "Eleven Fifty"
+
+let appName: string = 'Twitter';
+appName = "Instagram";
+
+let isLoggedIn: boolean = true;
+let isGoldLevelUser: boolean = true;
+
+let studentList: string[] = ['Tom', 'Tracy', 'Nikita']
+let allScores: Array<number> = [100, 99, 95, 96]
+
+console.log('Student List:', studentList[0])
+
+let dataFromThirdParty: any= '12345';
+console.log(dataFromThirdParty)
+console.log(typeof dataFromThirdParty)
+
+dataFromThirdParty = 12345;
+console.log(dataFromThirdParty)
+console.log(typeof dataFromThirdParty)
+
+function sayHelloToAll() : void {
+    console.log("Hello to All!")
+}
+
+let undefinedVariable: undefined = undefined
+let nullVariable: null = null;
+
+let usernameAndId: [number, string];
+
+usernameAndId = [1, 'kennis']
+
+enum WeaponType {Sword, Saber, Spear}
+let weapon: WeaponType = WeaponType.Saber
+
+enum CardType{Ace = 1, Two, Three, Four}
+let cardType: string=CardType[1]
+console.log(cardType)
--- js/01-types.js
+++ js/01-types.js
@@ -7,5 +7,45 @@ var email;
 var socialSecurity;
 var isMarried;
 var greeting = "Hello";
-password = 1234;
+// password = 1234;
+var age = 40;
+var currentYear = 2018;
+var companyName;
+companyName = "Eleven Fifty";
+var appName = 'Twitter';
+appName = "Instagram";
+var isLoggedIn = true;
+var isGoldLevelUser = true;
+var studentList = ['Tom', 'Tracy', 'Nikita'];
+var allScores = [100, 99, 95, 96];
+console.log('Student List:', studentList[0]);
+var dataFromThirdParty = '12345';
+console.log(dataFromThirdParty);
+console.log(typeof dataFromThirdParty);
+dataFromThirdParty = 12345;
+console.log(dataFromThirdParty);
+console.log(typeof dataFromThirdParty);
+function sayHelloToAll() {
+    console.log("Hello to All!");
+}
+var undefinedVariable = undefined;
+var nullVariable = null;
+var usernameAndId;
+usernameAndId = [1, 'kennis'];
+var WeaponType;
+(function (WeaponType) {
+    WeaponType[WeaponType["Sword"] = 0] = "Sword";
+    WeaponType[WeaponType["Saber"] = 1] = "Saber";
+    WeaponType[WeaponType["Spear"] = 2] = "Spear";
+})(WeaponType || (WeaponType = {}));
+var weapon = WeaponType.Saber;
+var CardType;
+(function (CardType) {
+    CardType[CardType["Ace"] = 1] = "Ace";
+    CardType[CardType["Two"] = 2] = "Two";
+    CardType[CardType["Three"] = 3] = "Three";
+    CardType[CardType["Four"] = 4] = "Four";
+})(CardType || (CardType = {}));
+var cardType = CardType[1];
+console.log(cardType);
 //# sourceMappingURL=01-types.js.map
