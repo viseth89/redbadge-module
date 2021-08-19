091ef70e35962810639f897ca649a2661350a819
viseth89 <viseth89@gmail.com>
2021-08-19 15:49:38 -0500

    Walkthrough with 2.06

---
+++ app/02-functions.ts
@@ -0,0 +1,14 @@
+function addNumbers(numOne: number, numTwo: number){
+    return numOne + numTwo
+}
+
+addNumbers(1,2);
+// addNumbers(1,2,3 );
+// addNumbers(1,2,3, 'Food');
+
+function sayHello(name:string) : string {
+    return name;
+}
+
+sayHello("Kenn")
+// sayHello(1)
--- js/02-functions.js
+++ js/02-functions.js
@@ -1,1 +1,12 @@
+function addNumbers(numOne, numTwo) {
+    return numOne + numTwo;
+}
+addNumbers(1, 2);
+// addNumbers(1,2,3 );
+// addNumbers(1,2,3, 'Food');
+function sayHello(name) {
+    return name;
+}
+sayHello("Kenn");
+// sayHello(1)
 //# sourceMappingURL=02-functions.js.map
